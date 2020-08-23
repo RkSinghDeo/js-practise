9924bcebb35d5dd1957da35a5308c008c165b2c9
RkSinghDeo <61618844+RkSinghDeo@users.noreply.github.com>
2020-08-23 23:05:13 +0530

    Add files via upload

--- promis_pro_3.O.js
+++ promis_pro_3.O.js
@@ -1,3 +1,4 @@
+//this funtion takes message as argument and just print msg after 2 seconds and returns promise
 function printafter2sec(msg)
 {
     return new Promise((resolve,reject)=>{
@@ -10,6 +11,7 @@ function printafter2sec(msg)
         })
     })
 }
+//this fuction takes a number(time in ms) as an input used as argument in setTimeout fuction and returns promise
 function getTheTimeNow (timeToAdd)
 {
    return  new Promise ((resolve,reject )=>{setTimeout(()=>{
@@ -19,8 +21,11 @@ function getTheTimeNow (timeToAdd)
    },timeToAdd);
 });
 };
+//this async funcion takes number(time in sec which is attcualy not real seconds) turn them into logical argument for getTheTimeNow funtion 
+//it prints the  resolved promise return from getTheTimeNow function
+//it awaits until the promise is resolved before excuting next task
 
-  async function printResult(time)
+async function printResult(time)
 {
    const timeToAdd=time*1000;
    await getTheTimeNow(timeToAdd)
@@ -28,8 +33,8 @@ function getTheTimeNow (timeToAdd)
    {
       console.log(result);
    })
-   //console.log(result);
 };
+//this array contains the functions as its elemets along with there required arguments
 var functionArray=
 [
   { fn: printResult,para: 1},
@@ -39,6 +44,12 @@ var functionArray=
   { fn: printResult,para: 4}
 
 ];
+
+//this async  function takes array as its argument
+//it executes each element of array in series and makes the next element await until the first element is executed
+// basically it changes a set of async funtions to execute as sync way even being asyc itself
+//thinking : it calls the next element only if the promise of its prevoius element is resolved
+//algorithm : it awaits for the promise to resolve then proceeds calling next element
   async  function executeArray(funAry) 
    {
       
--- promis_pro_3.O_without_async_await.js
+++ promis_pro_3.O_without_async_await.js
@@ -1,18 +1,23 @@
+//this fuction takes a number(time in ms) as an input used as argument in setTimeout fuction and returns promise
 function getTheTimeNow (timeToAdd)
 {
-   return  new Promise ((resolve,reject)=>{setTimeout(()=>{
-      let dateNow= new Date();
-      resolve(`print after ${timeToAdd/1000} seconds-->${dateNow}`);
+   return  new Promise ((resolve,reject)=>{setTimeout(()=>{ //defining new promise 
+      let dateNow= new Date();//current date and time
+      resolve(`print after ${timeToAdd/1000} seconds-->${dateNow}`)// statment to be return after being resolved
 
    },timeToAdd);
 });
 };
- 
+//this funcion takes number(time in sec which is attcualy not real seconds) turn them into logical argument for..
+//getTheTimeNow funtion and returns promise
+
  function printResult(time)
 {
-   const timeToAdd=time*1000;
-   return getTheTimeNow(timeToAdd);
+   const timeToAdd=time*1000;//coversion to logical argument
+   return getTheTimeNow(timeToAdd);//returning the result
 };
+
+//this funtion takes message as argument and just print msg after 2 seconds and returns promise
 function printafter2sec(msg)
 {
     return new Promise((resolve,reject)=>{
@@ -24,7 +29,8 @@ function printafter2sec(msg)
 
         })
     })
-}
+
+//this array contains the functions as its elemets along with there required arguments
 var functionArray=
 [
   { fn: printResult,para: 1},
@@ -34,19 +40,21 @@ var functionArray=
   { fn: printResult,para: 4}
 
 ];
-let  i=0;
 
-   function executeArray(funAry,i=0) 
-    
-{   
-    if(i<5)
-    {
-    funAry[i].fn(funAry[i].para).then((result)=>{
-    console.log(result);
-    executeArray(functionArray,++i);
-    }
-    )
-    }
+
+//this function takes array as its argument and have 1 defaut argument as well
+//it executes each element of array in series and makes the next element await until the first element is executed
+// basically it changes a set of async funtions to execute as sync way
+//thinking : it calls the next element only if the promise of its prevoius element is resolved
+//algorithm : it calls back itself after the promise is resolved with updated defaut i argument
+function executeArray(funAry=[],i=0) 
+{      
+    if(i<funAry.length)//condition to stop it after executing last element
+        {
+        funAry[i].fn(funAry[i].para).then((result)=>{
+        console.log(result);
+     executeArray(functionArray,++i);})
+        }
 }
 
  
